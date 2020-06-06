feddc98da15f7988db5562cd8d5f0673c8da7c8b
fbonet <fbonetvi@uoc.edu>
2020-06-06 20:19:20 +0200

    5

--- Fotos2014.js
+++ Fotos2014.js
@@ -4,9 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		alert("hola1");
 	};    
 
-	document.querySelector('#a').onclick = function() {
-		alert("hola3");
-	}; 
+
 	document.querySelector('.dropdown-item').onclick = function() {
 		alert("hola4");
 	}; 
