6cec4d6d740ec81cf97077f2d420cdaa200ed0f2
fbonet <fbonetvi@uoc.edu>
2020-06-07 12:37:36 +0200

    titol a sota

--- Fotos2014.js
+++ Fotos2014.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		         .selectAll("text")  
             		.style("text-anchor", "end")
             		.attr("dx", "-.8em")
-            		.attr("dy", "-.1em")
+            		.attr("dy", "-.5em")
             		.attr("transform", "rotate(-90)" );
 
 		        g.append("g")
