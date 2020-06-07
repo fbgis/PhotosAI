ed0a76fe464b35727e1a6beb9364415bfe5d8eb0
fbonet <fbonetvi@uoc.edu>
2020-06-07 13:00:19 +0200

    titol a sota

--- Fotos2014.js
+++ Fotos2014.js
@@ -49,6 +49,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		         .data(data)
 		         .enter().append("rect")
 		         .attr("class", "bar")
+		         .on("mouseover", onMouseOver)
+				 .on("mouseout", onMouseOut)
 		         .attr("x", function(d) { return xScale(d.column); })
 		         .attr("y", function(d) { return yScale(d.value); })
 		         .attr("width", xScale.bandwidth())
@@ -64,4 +66,42 @@ document.addEventListener('DOMContentLoaded', () => {
 		    });			
 		};	
 	}); 
-});    
+});   
+
+//mouseover event handler function
+function onMouseOver(d, i) {
+    d3.select(this).attr('class', 'highlight');
+    d3.select(this)
+      .transition()     // adds animation
+      .duration(400)
+      .attr('width', x.bandwidth() + 5)
+      .attr("y", function(d) { return y(d.value) - 10; })
+      .attr("height", function(d) { return height - y(d.value) + 10; });
+
+    g.append("text")
+     .attr('class', 'val') 
+     .attr('x', function() {
+         return x(d.year);
+     })
+     .attr('y', function() {
+         return y(d.value) - 15;
+     })
+     .text(function() {
+         return [ '$' +d.value];  // Value of the text
+     });
+}
+
+//mouseout event handler function
+function onMouseOut(d, i) {
+    // use the text label class to remove label on mouseout
+    d3.select(this).attr('class', 'bar');
+    d3.select(this)
+      .transition()     // adds animation
+      .duration(400)
+      .attr('width', x.bandwidth())
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); });
+
+    d3.selectAll('.val')
+      .remove()
+} 
