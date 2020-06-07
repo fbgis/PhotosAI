6f221deebc444803b09b16b027aa8c83eabd5016
fbonet <fbonetvi@uoc.edu>
2020-06-07 18:32:32 +0200

    titol a sota

--- Fotos2014.js
+++ Fotos2014.js
@@ -54,14 +54,14 @@ document.addEventListener('DOMContentLoaded', () => {
 				 .on("mouseout", onMouseOut)
 		         .attr("x", function(d) { return xScale(d.column); })
 		         .attr("y", function(d) { return yScale(d.value); })
-		         .attr("width", x.bandwidth())
+		         .attr("width", xScale.bandwidth())
 		         .transition()
 		         .ease(d3.easeLinear)
 		         .duration(200)
 		         .delay(function (d, i) {
 		             return i * 50;
 		         })
-		         .attr("height", function(d) { return height - yScale(d.value); });
+		         .attr("height", d => yScale(0) - yScale(d.value));
 
 				// g.selectAll(".bar")
 				// 	.append("text")
